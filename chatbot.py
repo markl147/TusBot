e3be57df6ead1c12b581c6435f41b9c939a27529
Mark <lohanmark@yahoo.co.uk>
2023-04-12 18:17:23 +0100

    added functionality to collect user feedback rather than automatically update the model.. this will be used so the admin can see the questions and answers after and manually update the model

--- chatbot.py
+++ chatbot.py
@@ -2,6 +2,18 @@
 import numpy as np
 import tensorflow as tf
 import pickle
+import pandas as pd
+
+# Save the users question, the incorrect response as designated by the user, and the users feedback, adds them to a list and when the program ends exports them to a csv file
+def export_feedback_to_csv(user_question, bad_bot_response, user_response):
+    data_dict = {'question': user_question_list, 'response': bot_response_list, 'feedback': user_answer_list}
+    data_df = pd.DataFrame(data_dict)
+    data_df.to_csv('user_feedback.csv', index=False)
+
+# Initialize lists to store questions, responses, and feedback
+user_question_list = []
+bot_response_list = []
+user_answer_list = []
 
 # Load the tokenizer
 with open('tokens/tokenizer.pickle', 'rb') as handle:
@@ -49,10 +61,20 @@ while True:
     print('Bot:', response)
 
     # Ask for user feedback
-    feedback = input('Is this response good or bad? (y/n): ')
+    feedback = input('Is this response satisfactory? (y/n): ')
     if feedback == 'n':
         proper_answer = input('Please provide the correct response: ')
-        user_update_model(question, proper_answer)
 
-model.save('models/model_user_updated.h5')
-print("model updated")
+        #calls the method to update model with correct answer for question
+        #user_update_model(question, proper_answer)
+
+        # Store the question, response, and feedback
+        user_question_list.append(question)
+        bot_response_list.append(response)
+        user_answer_list.append(proper_answer)
+
+#trains the model on questions that users provided the correct responses for
+# model.save('models/model_user_updated.h5')
+# print("model updated")
+
+export_feedback_to_csv(user_question_list, bot_response_list, user_answer_list)
